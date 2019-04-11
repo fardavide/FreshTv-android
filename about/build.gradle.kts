c2cdae6df51b8c4afcd8c753957cc53572fbe616
4face-studi0 <37957092+4face-studi0@users.noreply.github.com>
2019-04-11 18:28:55 +0200

    Missing files

---
+++ about/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("com.android.library" )
+    id("kotlin-android" )
+    id("kotlin-android-extensions" )
+}
+
+android { applyAndroidConfig() }
+
+dependencies {
+    implementation( project(":commonandroid" ) )
+
+    applyTests()
+    applyAndroidTests()
+}
