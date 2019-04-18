203a7397052b151c81192254e426e7c6e504dd86
4face-studi0 <37957092+4face-studi0@users.noreply.github.com>
2019-04-18 19:37:56 +0200

    Incremented build number

--- buildSrc/src/main/java/Project.kt
+++ buildSrc/src/main/java/Project.kt
@@ -22,7 +22,7 @@ object Project {
     private val minor:      Int =       0
     private val channel:    Channel =   Alpha
     private val patch:      Int =       8
-    private val build:      Int =       5
+    private val build:      Int =       6
 
 
     // ====================   S T A T I C   C O D E   ==================== //
